8237887c285014e7462d627b529dd1bbc6070d62
Cross Cantiberos <cantiberos.crossiv@gmail.com>
2025-06-10 10:47:20 -0700

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,11 @@
+
+number = 10
+if number>15:
+   print("Number is Greater Than 15")
+else :
+    print("Number is Less Than 15")
+age = input("Enter Your Age: ")
+if age>"18":
+   print("You Are Eligible for a Drivers License")
+else :
+    print("You Are Not Eligible for a Drivers License")
