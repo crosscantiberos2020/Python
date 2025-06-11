e37fb17539ec390f43d79680ebe8bf6a3fb67efc
Cross Cantiberos <cantiberos.crossiv@gmail.com>
2025-06-11 10:47:52 -0700

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,39 @@
+age = 14
+if(age>18) :
+   print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+day = 99
+
+if day == 1 :
+    print("Monday")
+elif day == 2 :
+    print("Tuesday")
+elif day == 3 :
+    print("Wednesday")
+elif day == 4 :
+    print("Thursday")
+elif day == 5 :
+    print("Friday")
+elif day == 6 :
+    print("Saturday")
+elif day == 7 :
+    print("Sunday")
+else:
+    print("Invalid Entry")
+
+
+name = input("Enter your Username: ")
+password = input("Enter your Password: ")
+if password == "1234" :
+   print("Welcome")
+else: 
+     print("Invalid")
+
+
+if name == "cross" :
+   print("Valid User")
+else: 
+    print("Access Denied")
+
