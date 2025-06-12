52faaaa163bd9b61590cd92162276790bbacfa85
Cross Cantiberos <cantiberos.crossiv@gmail.com>
2025-06-12 10:39:40 -0700

    Add files via upload

---
+++ Lesson4.py
@@ -0,0 +1,20 @@
+# for c in range(1,21):
+#     print(c, "Hello")
+
+# number = 1
+# while (number<15):
+#     print(number)
+#     number = number+1
+
+# print("Outside the loop")
+
+# number = int(input("Enter a number "))
+# if number%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+for c in range(1, 101):
+    if c%2!=0:
+        print(c)
+        
